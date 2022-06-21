fd45aa5fc022d512963322f5cb3bef98a8874032
collins felix <cllnsflx40@gmail.com>
2022-06-21 15:29:43 -0700

    worked on sending each user cart product

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -25,5 +25,40 @@ const fectchUserProducts = async (req, res) => {
     }
 
 }
+const deleteone = async (req, res) => {
+    const {id, userId} = req.params 
+    console.log(id)
+    console.log(userId)
+    try {
+        const existingUser = await shppingcart.deleteOne({userid: userId, id: id})
+        if (!existingUser) {
+            return res.status(400).status({message: "cant find user"})
+        }
+        // const product = existingUser.deleteOne({id})
+
+        // product.deleteOne()
+        return res.status(200).json({message: "successfully deleted"})
+    } catch (error) {
+        console.log(error)
+    } 
+
+}
+const deleteAll = async (req, res) => {
+    const {userId} = req.params 
+
+    try {
+        const products = await shppingcart.deleteMany({userid: userId})
+        if (!existingUser) {
+            return res.status(400).status({message: "cant find user"})
+        }
+
+        return res.status(200).json({message: "All successfully deleted"})
+    } catch (error) {
+        console.log(error)
+    }
+
+}
 exports.addToCart2 = addToCart2
 exports.fectchUserProducts = fectchUserProducts
+exports.deleteone = deleteone
+exports.deleteAll = deleteAll
--- routes/cart.js
+++ routes/cart.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { addToCart2, fectchUserProducts } = require("../controllers/cartController");
+const { addToCart2, fectchUserProducts, deleteAll, deleteone } = require("../controllers/cartController");
 
 const cartrouter = express.Router();
 
 cartrouter.post("/addtocart", addToCart2)
 cartrouter.get("/fectchUserProducts/:id", fectchUserProducts)
+cartrouter.delete("/deleteAll/:userId", deleteAll)
+cartrouter.delete("/deleteOne/:id/:userId", deleteone)
 
 exports.cartrouter =cartrouter
